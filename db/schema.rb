5c938ff5034f1daa537ffbb2f43783c3e6affa6a
Syanom <yandupliy@gmail.com>
2021-10-28 15:07:51 +0300

    change user reference to author in test model

--- db/schema.rb
+++ db/schema.rb
@@ -39,11 +39,11 @@ ActiveRecord::Schema.define(version: 2021_10_28_101925) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
-    t.integer "user_id", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
-    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,6 +54,6 @@ ActiveRecord::Schema.define(version: 2021_10_28_101925) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "authors"
   add_foreign_key "tests", "categories"
-  add_foreign_key "tests", "users"
 end
