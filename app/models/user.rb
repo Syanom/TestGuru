b238febd15c09400df7dd41f9cfe43f9672fc3d6
Syanom <yandupliy@gmail.com>
2021-10-30 02:33:28 +0300

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  def tests_by_level(level)
+    ids = TestCompletion.joins('JOIN tests ON test_completions.test_id = tests.id')
+                        .where(tests: { level: level }, user_id: id).pluck('test_id')
+    answer = []
+    ids.each { |id| answer << Test.find_by(id: id) }
+    answer
+  end
 end
