bf3419dd090ea5474a3cf6826f71fbbbd762a8df
Syanom <yandupliy@gmail.com>
2021-10-30 04:10:04 +0300

    refactor tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    ids = TestCompletion.joins('JOIN tests ON test_completions.test_id = tests.id')
-                        .where(tests: { level: level }, user_id: id).pluck('test_id')
-    answer = []
-    ids.each { |id| answer << Test.find_by(id: id) }
-    answer
+    Test.joins('JOIN test_completions ON test_completions.test_id = tests.id')
+        .where(tests: { level: level }, test_completions: { user_id: id })
   end
 end
