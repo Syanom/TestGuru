13ff6c32d5532f3f7adea55d90cba1d840568028
Syanom <yandupliy@gmail.com>
2021-11-04 16:33:32 +0300

    first iteration on task

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  has_many :questions
+  has_many :test_completions
+  has_many :users, through: :test_completions
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+
   def self.tests_by_category(category_name)
     joins('JOIN categories ON tests.category_id = categories.id')
       .where(categories: { name: category_name })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :tests
+  has_many :test_completions
+  has_many :completed_tests, class_name: 'Test', through: :test_completions, source: :test
+
   def tests_by_level(level)
     Test.joins('JOIN test_completions ON test_completions.test_id = tests.id')
         .where(tests: { level: level }, test_completions: { user_id: id })
