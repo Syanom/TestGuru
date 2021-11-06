bd66c2c02986271be82d8fbad105360cd1be014e
Syanom <yandupliy@gmail.com>
2021-11-06 14:18:34 +0300

    codestyle improvements

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+
   has_many :questions, dependent: :destroy
   has_many :test_completions, dependent: :destroy
   has_many :users, through: :test_completions
-
-  belongs_to :category
-  belongs_to :author, class_name: 'User'
 
   def self.tests_by_category(category_name)
     joins('JOIN categories ON tests.category_id = categories.id')
