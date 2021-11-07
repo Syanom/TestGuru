148a0afb3c2f05ddeaacf4ad49333b3db50d6c3d
Syanom <yandupliy@gmail.com>
2021-11-07 21:05:50 +0300

    fix answer amount validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,19 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :answers_amount, on: :create
 
   scope :correct, -> { where(correct: true) }
+
+  def destroy
+    raise "You can't delete the only answer of the question" unless question.answers.count > 1
+
+    super
+  end
+
+  private
+
+  def answers_amount
+    errors.add(:answers_amount, "You can't add more than 4 answers to a question") unless question.answers.count < 4
+  end
 end
