5442a15daecdeb87465eb56ec800eb78306289d8
Syanom <yandupliy@gmail.com>
2021-11-12 18:35:02 +0300

    add find test, question and routes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,14 @@
 class QuestionsController < ApplicationController
+  before_action :find_test
+  before_action :find_question
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
 end
