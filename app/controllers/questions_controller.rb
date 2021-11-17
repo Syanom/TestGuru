98dae836ee74d5025d370c7a09aeec51e7a0140c
Syanom <yandupliy@gmail.com>
2021-11-17 23:16:49 +0300

    add create question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,29 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
-  before_action :find_question
+  before_action :find_test, only: %i[index create]
+  before_action :find_question, only: %i[show destroy]
+
+  def index
+    render plain: @test.questions.pluck(:body).join("\n")
+  end
+
+  def show
+    render plain: @question.attributes
+  end
+
+  def destroy
+    @question.destroy
+  end
+
+  def new; end
+
+  def create
+    @question = Question.new(body: params[:question_body], test_id: @test.id)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
 
   private
 
