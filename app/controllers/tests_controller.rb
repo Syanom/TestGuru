ab7339344d82544df57c19aae8acd2d755d8411e
Syanom <yandupliy@gmail.com>
2021-11-22 18:50:35 +0300

    create tests methods

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,12 +1,43 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show]
-  before_action :find_questions, only: %i[show]
+  before_action :find_test, only: %i[show edit update destroy]
 
   def index
     @tests = Test.all
   end
 
-  def show; end
+  def show
+    @questions = @test.questions
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+
+    redirect_to tests_path
+  end
 
   private
 
@@ -14,7 +45,7 @@ class TestsController < ApplicationController
     @test = Test.find(params[:id])
   end
 
-  def find_questions
-    @questions = @test.questions
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end
