c2c17267b8c377b46885c27fc8a66d0d11eab7ab
Syanom <yandupliy@gmail.com>
2021-11-24 14:50:53 +0300

    create answers routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :tests do
-    resources :questions, except: :index, shallow: true
+    resources :questions, except: :index, shallow: true do
+      resources :answers, shallow: true
+    end
   end
 end
