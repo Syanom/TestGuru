72c749cb58c32af8e61c33f4d8bc0c4954755772
Syanom <yandupliy@gmail.com>
2021-12-03 02:25:42 +0300

    Admin resources lesson

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,57 +1,13 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_test, only: %i[show edit update destroy start]
 
   def start
+    @test = Test.find(params[:id])
     current_user.tests.push(@test)
     redirect_to current_user.test_completion(@test)
   end
 
   def index
     @tests = Test.all
-  end
-
-  def show
-    @questions = @test.questions
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = current_user.authored_tests.build(test_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-
-    redirect_to tests_path
-  end
-
-  private
-
-  def find_test
-    @test = Test.find(params[:id])
-  end
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, except: :index, shallow: true do
       resources :answers, except: :index, shallow: true
     end
@@ -19,5 +19,9 @@ Rails.application.routes.draw do
     member do
       get :result
     end
+  end
+
+  namespace :admin do
+    resources :tests
   end
 end
