3746fb1ea8b100ce4414f7f7defd7bfd33b37ac0
Syanom <yandupliy@gmail.com>
2021-12-03 19:00:30 +0300

    delete question now will not delete test_completion

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
-  has_many :test_completions, foreign_key: 'current_question_id', dependent: :destroy
+  has_many :test_completions, foreign_key: 'current_question_id', dependent: :nullify
 
   validates :body, presence: true
 end
