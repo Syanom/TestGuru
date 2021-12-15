890a472f08788b39dc2f5de280bed261fa5acee4
Syanom <yandupliy@gmail.com>
2021-12-15 21:00:45 +0300

    remove unused code

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,5 @@ module TestGuru
     config.i18n.default_locale = :ru
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.autoload_paths << "#{Rails.root}/lib/clients"
   end
 end
