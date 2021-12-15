38eecef61f018027c86fa7bcd5f47ef8b7b2a353
Syanom <yandupliy@gmail.com>
2021-12-15 20:56:58 +0300

    refactor gist_content

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -23,9 +23,7 @@ class GistQuestionService
   end
 
   def gist_content
-    content = [@question.body]
-    content += @question.answers.pluck(:body)
-    content.join("\n")
+    [@question.body, *@question.answers.pluck(:body)].join("\n")
   end
 
   def setup_http_client
