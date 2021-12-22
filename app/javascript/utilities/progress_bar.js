be60be16ef72d6425bc2b71efb60acc45d41239e
Syanom <yandupliy@gmail.com>
2021-12-22 20:11:48 +0300

    add progress bar

---
+++ app/javascript/utilities/progress_bar.js
@@ -0,0 +1,15 @@
+document.addEventListener('turbolinks:load', function () {
+  const progressBar = document.querySelector('.progress-bar')
+  console.log(progressBar)
+  if (progressBar) {
+    setProgress(progressBar)
+  }
+})
+
+function setProgress(progressBar) {
+  const questionNumber = progressBar.dataset.questionNumber
+  const questionsQuantity = progressBar.dataset.questionsQuantity
+  const progress = Math.round(questionNumber / questionsQuantity * 100)
+  progressBar.ariaValueNow = progress
+  progressBar.style.width = progress + '%'
+}
