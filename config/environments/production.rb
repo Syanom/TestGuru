57b686c0a6ddeafa12ea21534fdf2328b245b636
Syanom <yandupliy@gmail.com>
2021-12-24 15:55:17 +0300

    try fix mail sending2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user: ENV['SMTP_USERNAME'],
+    username: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
