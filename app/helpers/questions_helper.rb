ce3e5e3118269d8c7fe64fded2a4ba4b9c3de339
Syanom <yandupliy@gmail.com>
2021-12-28 14:50:24 +0300

    translate questions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,9 @@
 module QuestionsHelper
   def question_header
     if @question.new_record?
-      "Create New #{@question.test.title} Question"
+      t('admin.questions.for_helper.create_new_question', test_title: @question.test.title)
     else
-      "Edit #{@question.test.title} Question"
+      t('admin.questions.for_helper.edit_question', body: @question.body)
     end
   end
 end
