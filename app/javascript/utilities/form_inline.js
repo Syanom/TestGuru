f13e1094530cf0bc8a3f302c69e685f03f7e6e2d
Syanom <yandupliy@gmail.com>
2022-01-02 22:14:49 +0300

    refactor form_inline to be more abstract

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -17,23 +17,24 @@ document.addEventListener('turbolinks:load', function () {
 function formInlineClickHandler(event) {
   event.preventDefault()
 
-  const testId = this.dataset.testId
-  formInlineHandler(testId)
+  const itemId = this.dataset.itemId
+  console.log(itemId)
+  formInlineHandler(itemId)
 }
 
-function formInlineHandler(testId) {
-  const editLink = document.querySelector('.form-inline-edit-link[data-test-id="' + testId + '"')
-  const cancelLink = document.querySelector('.form-inline-cancel-link[data-test-id="' + testId + '"')
-  const testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"')
-  const formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"')
+function formInlineHandler(itemId) {
+  const editLink = document.querySelector('.form-inline-edit-link[data-item-id="' + itemId + '"')
+  const cancelLink = document.querySelector('.form-inline-cancel-link[data-item-id="' + itemId + '"')
+  const itemTitle = document.querySelector('.item-title[data-item-id="' + itemId + '"')
+  const formInline = document.querySelector('.form-inline[data-item-id="' + itemId + '"')
 
   if (formInline.classList.contains('hide')) {
-    testTitle.classList.add('hide')
+    itemTitle.classList.add('hide')
     formInline.classList.remove('hide')
     editLink.classList.add('hide')
     cancelLink.classList.remove('hide')
   } else {
-    testTitle.classList.remove('hide')
+    itemTitle.classList.remove('hide')
     formInline.classList.add('hide')
     editLink.classList.remove('hide')
     cancelLink.classList.add('hide')
