4364b96913f44636cf689ad2ba0bc4e6c2f64346
Syanom <yandupliy@gmail.com>
2022-01-07 22:37:38 +0300

    add time spent

--- app/services/timer_service.rb
+++ app/services/timer_service.rb
@@ -1,5 +1,9 @@
 class TimerService
-  attr_reader :time_left
+  attr_reader :time_left, :time_spent
+
+  def initialize
+    @time_spent = 0
+  end
 
   def start_timer(seconds)
     @time_left = seconds
@@ -7,6 +11,7 @@ class TimerService
       seconds.downto(1) do
         sleep 1
         @time_left -= 1
+        @time_spent += 1
       end
     end
   end
