7c222f82a3956231f5c11f2fe6d6e01355c3d535
Syanom <yandupliy@gmail.com>
2022-02-10 18:57:08 +0300

    badge form translation

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,2 +1,9 @@
 module BadgesHelper
+  def badge_header
+    if @badge.new_record?
+      t('admin.badges.for_helper.create_new_badge')
+    else
+      t('admin.badges.for_helper.edit_badge', name: @badge.name)
+    end
+  end
 end
