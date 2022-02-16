231231b141568173e4de34f0c0cdf446754221dc
Syanom <yandupliy@gmail.com>
2022-02-16 17:40:49 +0300

    refactor previous commit to make code cleaner

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,6 +1,5 @@
 class BadgesController < ApplicationController
   def index
-    @earned_badges = Badge.select { |badge| badge.users.include?(current_user) }
-    @unearned_badges = Badge.all - @earned_badges
+    @unearned_badges = Badge.all - Badge.select { |badge| badge.users.include?(current_user) }
   end
 end
