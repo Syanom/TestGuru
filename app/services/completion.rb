183eeb1a268055e579479ccfd53bed1501ed85ea
Syanom <yandupliy@gmail.com>
2022-02-16 19:09:01 +0300

    what a weird code

--- app/services/completion.rb
+++ app/services/completion.rb
@@ -1,13 +1,7 @@
 class Completion < AbstractBadgeRule
   def count_badge_allotments
-    result = Array.new(@badge.tests.length, 0)
-    i = 0
-    @badge.tests.each do |test|
-      test.test_completions.where(user: @user).each do |test_completion|
-        result[i] += 1 if test_completion.successful?
-      end
-      i += 1
-    end
-    result.min
+    @badge.tests.min do |test|
+      test.test_completions.where(user: @user).count(&:successful?)
+    end.test_completions.where(user: @user).count(&:successful?)
   end
 end
