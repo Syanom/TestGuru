dc87873e585878fbb866f319296c8225513cb705
Syanom <yandupliy@gmail.com>
2022-02-16 18:43:53 +0300

    some refactoring

--- app/services/attempts.rb
+++ app/services/attempts.rb
@@ -2,14 +2,11 @@ class Attempts < AbstractBadgeRule
   def count_badge_allotments
     result = 1
     @badge.tests.each do |test|
-      failed_attempts = 0
-      successful = false
       test_completions = test.test_completions.where(user: @user)
-      test_completions.each do |test_completion|
-        failed_attempts += 1 unless test_completion.successful?
-        successful = true if test_completion.successful?
+      failures = test_completions.count { |test_completion| !test_completion.successful? }
+      if failures >= @badge.rule_value.to_i || test_completions.empty? || test_completions.none?(&:successful?)
+        result = 0
       end
-      result = 0 if failed_attempts >= @badge.rule_value.to_i || test_completions.empty? || !successful
     end
     result
   end
